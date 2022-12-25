f525c6e903e3e2c28a14f2262ef909333821178e
Your Name <you@example.com>
2022-12-25 21:16:43 +0530

    Added: Retrieve employee payroll from database

---
+++ src/main/java/com/bridgelabz/EmployeePayrollData.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+import java.time.LocalDate;
+
+public class EmployeePayrollData {
+
+    public int id;
+    public String name;
+    public double basic_pay;
+    public LocalDate start;
+
+    public EmployeePayrollData(int id, String name, double basic_pay, LocalDate start) {
+        this.id = id;
+        this.name = name;
+        this.basic_pay = basic_pay;
+        this.start = start;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeePayrollData{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", basic_pay=" + basic_pay +
+                ", start=" + start +
+                '}';
+    }
+}
---
+++ src/test/java/com/bridgelabz/EmployeePayrollTest.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class EmployeePayrollTest {
+
+    @Test
+
+    /*
+     * TestCase to check number of employees in the sql table by using readEmployeePayrollData method
+     * and save number of employee in arrayList of employeePayrollData type
+     */
+    public void givenEmpPayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+        EmployeePayroll employeePayroll = new EmployeePayroll();
+        List<EmployeePayrollData> employeePayrollData = employeePayroll.readEmployeePayrollData();
+        Assertions.assertEquals(0, employeePayrollData.size());
+    }
+}
